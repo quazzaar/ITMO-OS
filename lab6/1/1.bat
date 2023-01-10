4a4c567440ea1129fee0d31c636503a988454c03
Dmitriy <89843200+quazzaar@users.noreply.github.com>
2023-01-10 16:24:49 +0300

    Update 1.bat

--- lab6/1/1.bat
+++ lab6/1/1.bat
@@ -1,4 +1,4 @@
 wmic os get caption, version > version.txt
-wmic computersystem get totalphysicalmemory > FreeAndTotalMemory.txt
-wmic os get freephysicalmemory >> FreeAndTotalMemory.txt
+wmic computersystem get totalphysicalmemory > totalmemory.txt
+wmic os get freephysicalmemory >> freeemory.txt
 wmic logicaldisk list brief > disks.txt
